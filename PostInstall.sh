d78d5951ea7499372831bfb778223ad6e21b2e42
Harry Carey <harry.carey95@gmail.com>
2023-02-06 17:48:25 +0100

    Build Image magick on the server due to openshift being difficult

--- PostInstall.sh
+++ PostInstall.sh
@@ -1,7 +1,16 @@
 mkdir runningJobs
-# # check if running in local or remote
-# if ["$env"]; then
-#     echo "local"
+# check if running in local or remote
+if ["$env"]; then
+    echo "local"
+else 
+    wget https://github.com/ImageMagick/ImageMagick/archive/refs/tags/7.1.0-61.tar.gz
+    tar -xvf 7.1.0-61.tar.gz
+    cd ImageMagick-7.1.0-61
+    ./configure
+    make
+    make install
+fi
+
 # else   
 #     wget https://download.oracle.com/java/19/latest/jdk-19_linux-x64_bin.tar.gz   
 #     tar -xvf jdk-19_linux-x64_bin.tar.gz 
