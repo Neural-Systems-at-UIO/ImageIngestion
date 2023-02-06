50e504ab97a45d69e2315d293a725b5a49102fa0
Harry Carey <harry.carey95@gmail.com>
2023-02-06 13:25:04 +0100

    added identify binary file

--- PostInstall.sh
+++ PostInstall.sh
@@ -1,11 +1,11 @@
 mkdir runningJobs
-# check if running in local or remote
-if ["$env"]; then
-    echo "local"
-else   
-    wget https://download.oracle.com/java/19/latest/jdk-19_linux-x64_bin.tar.gz   
-    tar -xvf jdk-19_linux-x64_bin.tar.gz 
-    wget https://imagemagick.org/archive/binaries/magick --no-check-certificate
-        # 
-fi
+# # check if running in local or remote
+# if ["$env"]; then
+#     echo "local"
+# else   
+#     wget https://download.oracle.com/java/19/latest/jdk-19_linux-x64_bin.tar.gz   
+#     tar -xvf jdk-19_linux-x64_bin.tar.gz 
+#     wget https://imagemagick.org/archive/binaries/magick --no-check-certificate
+#         # 
+# fi
 
