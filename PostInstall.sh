561d5320bf815f34526557a82d12c63971b925a4
Harry Carey <harry.carey95@gmail.com>
2023-02-06 19:38:42 +0100

    added jdk to the postinstall script

--- PostInstall.sh
+++ PostInstall.sh
@@ -9,12 +9,9 @@ else
     ./configure --prefix=$HOME/
     make
     make install
+    wget https://download.oracle.com/java/19/latest/jdk-19_linux-x64_bin.tar.gz   
+    tar -xvf jdk-19_linux-x64_bin.tar.gz 
 fi
 
-# else   
-#     wget https://download.oracle.com/java/19/latest/jdk-19_linux-x64_bin.tar.gz   
-#     tar -xvf jdk-19_linux-x64_bin.tar.gz 
-#     wget https://imagemagick.org/archive/binaries/magick --no-check-certificate
-#         # 
-# fi
+
 
