a117610be8029f4b2943d2e89a9760cd1ff1c75f
polarbean <harry.carey95@gmail.com>
2023-06-03 16:12:14 +0200

    windows was adding crlf line endings causing the script to not run on the remote machine

--- PostInstall.sh
+++ PostInstall.sh
@@ -1,17 +1,14 @@
 mkdir runningJobs
 # check if running in local or remote
-if ["$env"]; then
+if [ "$env" ]; then
     echo "local"
-else 
+else
     wget https://download.oracle.com/java/20/latest/jdk-20_linux-x64_bin.tar.gz
-    tar -xvf jdk-19_linux-x64_bin.tar.gz 
+    tar -xvf jdk-20_linux-x64_bin.tar.gz 
     wget https://github.com/ImageMagick/ImageMagick/archive/refs/tags/7.0.8-12.tar.gz
     tar -xvf 7.0.8-12.tar.gz
     cd ImageMagick-7.0.8-12
     ./configure --prefix=$HOME/
     make
     make install
-
 fi
-
-
