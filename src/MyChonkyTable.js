4f9f119f53bb5e32658af6ffbc1c0bf5f5ba9c0e
Harry Carey <harry.carey95@gmail.com>
2023-07-03 12:30:27 +0200

    fixed issue where navigating via the top folderchain sometimes gave the wrong results

--- src/MyChonkyTable.js
+++ src/MyChonkyTable.js
@@ -1,7 +1,9 @@
-import { setChonkyDefaults , ChonkyActions, defineFileAction,   FileBrowser,
-    FileList,
-    FileNavbar,
-    FileToolbar} from "chonky";
+import {
+  setChonkyDefaults, ChonkyActions, defineFileAction, FileBrowser,
+  FileList,
+  FileNavbar,
+  FileToolbar
+} from "chonky";
 import { ChonkyIconFA } from "chonky-icon-fontawesome";
 import React, { useEffect, useState } from "react";
 import { ListBucketFiles, UploadFiles, DownloadFiles, DeleteFiles } from "./ButtonActions";
@@ -31,42 +33,42 @@ export const MyEmojiIcon = React.memo((props) => {
 
 
 const CreateBrainFromFiles = defineFileAction({
-    id: "GenerateBrain",
-    requiresSelection: false,
-  
-    button: {
-      name: " ",
-      toolbar: true,
-    },
-  });
-  
+  id: "GenerateBrain",
+  requiresSelection: false,
 
-  const UploadFolder = defineFileAction({
-    id: "UploadFolder",
-    requiresSelection: false,
-    button: {
-      name: "UploadFolder",
-      toolbar: true,
-    },
-  });
-  
+  button: {
+    name: " ",
+    toolbar: true,
+  },
+});
 
+
+const UploadFolder = defineFileAction({
+  id: "UploadFolder",
+  requiresSelection: false,
+  button: {
+    name: "UploadFolder",
+    toolbar: true,
+  },
+});
 
 
 
-  const myFileActions = [
-    CreateBrainFromFiles,
-    ChonkyActions.CreateFolder,
-    ChonkyActions.UploadFiles,
-    UploadFolder,
 
-    ChonkyActions.DownloadFiles,
-    ChonkyActions.DeleteFiles,
-    ChonkyActions.EnableListView,
-    ChonkyActions.SelectAllFiles,
-    ChonkyActions.ClearSelection,
-  ];
-  
+
+const myFileActions = [
+  CreateBrainFromFiles,
+  ChonkyActions.CreateFolder,
+  ChonkyActions.UploadFiles,
+  UploadFolder,
+
+  ChonkyActions.DownloadFiles,
+  ChonkyActions.DeleteFiles,
+  ChonkyActions.EnableListView,
+  ChonkyActions.SelectAllFiles,
+  ChonkyActions.ClearSelection,
+];
+
 const uploadFileAction = myFileActions.find(action => action.id === ChonkyActions.UploadFiles.id);
 uploadFileAction.button.group = "";
 const uploadFolderAction = myFileActions.find(action => action.id === UploadFolder.id);
@@ -93,156 +95,158 @@ const CreateBrainFromFilesAction = myFileActions.find(action => action.id === Cr
 
 
 function updateCurDirPath(curDirPath, SetCurDirPath) {
-    if (!curDirPath.endsWith("/")) {
-      curDirPath = curDirPath + "/";
-    }
-    if (curDirPath == "/")
-    {
-      curDirPath = "";
-    }
-    SetCurDirPath(curDirPath);
+  if (!curDirPath.endsWith("/")) {
+    curDirPath = curDirPath + "/";
   }
+  if (curDirPath == "/") {
+    curDirPath = "";
+  }
+  SetCurDirPath(curDirPath);
+}
 
 
 function createFileChain(targetFilePath) {
-    var FileChain = [{ id: '/', name: 'Home', isDir: true, openable:true},];
-    targetFilePath = targetFilePath.replace(/\/$/, "");
-    for (var i = 0; i < targetFilePath.length  ; i++) {
-      var temp = {
-        // id should be a join of the array up to the current index
-        id : targetFilePath.split("/").slice(0, i + 1).join("/"),
-        name : targetFilePath.split("/")[i],
-        isDir : true,
-        openable : true,
+  var FileChain = [{ id: '/', name: 'Home', isDir: true, openable: true },];
+  targetFilePath = targetFilePath.replace(/\/$/, "");
+  for (var i = 0; i < targetFilePath.length; i++) {
+    var temp = {
+      // id should be a join of the array up to the current index
+      id: targetFilePath.split("/").slice(0, i + 1).join("/"),
+      name: targetFilePath.split("/")[i],
+      isDir: true,
+      openable: true,
     }
-    FileChain[i+1] = temp;
-    }
-    return FileChain;
-    
+    FileChain[i + 1] = temp;
   }
+  return FileChain;
 
-
-
-  function FileActionHandler(data, SetFolderChain,curDirPath, SetCurDirPath, setFiles,bucket_name, selectedAtlas, token) {
-    // open a file picker UI
-    console.log('data', data)
-  
-    if (data.id == "open_files" && data.payload.targetFile.isDir) {
-      var targetFile = data.payload.targetFile;
-      var targetFileChain = createFileChain(targetFile.id);
-
-      SetFolderChain(targetFileChain);
-
-      curDirPath= targetFile.id;
-      if (curDirPath == "/")
-      {
-        curDirPath = "";
-      }
-      updateCurDirPath(curDirPath, SetCurDirPath);
-      
-
-
-      ListBucketFiles(
-        setFiles,
-        bucket_name,
-        curDirPath,
-        token
-      );
-    }
-  
-    if (data.id == "upload_files") {
-      // open
-
-      UploadFiles(curDirPath, setFiles, bucket_name,false, token);
-    
-    }
-    if (data.id == "UploadFolder") {
-      // open
-      UploadFiles(curDirPath, setFiles, bucket_name,true, token);
-    
-    }
-    // if (data.id == "create_folder") {
-    //   AddToItems(setItems, items, 'test_new', 'Processing') 
-    // }
-    if (data.id == "download_files") {
-      DownloadFiles(data, bucket_name, token);
-    }
-    if (data.id == "delete_files") {
-      DeleteFiles(data, curDirPath, setFiles,bucket_name, token);
-    }
-    if (data.id == "GenerateBrain") {
-      var selectedFiles = null;
-      // join all selected files with comma
-      for (var i = 0; i < data.state.selectedFilesForAction.length; i++) {
-        if (selectedFiles == null) {
-          selectedFiles = data.state.selectedFilesForAction[i].name;
-        } else {
-          selectedFiles =
-            selectedFiles + "," + data.state.selectedFilesForAction[i].name;
-        }
-      }
-      // set bucket name
-      // acquire brain ID from Input with ID brainID
-      var brainID = document.getElementById("brainIDInput").value;
-      // get the value of a select
-      console.log('brainID', brainID)
-      console.log('targetatlas', selectedAtlas)
-      
-      // request to generate brain
-      var xhr = new XMLHttpRequest();
-      let redirect_uri = process.env.REACT_APP_URL;
-
-      console.log("CurDIRPath: " + curDirPath);
-  if (process.env.NODE_ENV === 'development') {
-  xhr.open("GET", `${redirect_uri}/tiffListToTarDZI?bucketname=${bucket_name}&filelist=${selectedFiles}&brainID=${brainID}&folderName=${curDirPath}&selectedAtlas=${selectedAtlas}`, true);
-} else {
-  xhr.open("GET", `/tiffListToTarDZI?bucketname=${bucket_name}&filelist=${selectedFiles}&brainID=${brainID}&folderName=${curDirPath}&selectedAtlas=${selectedAtlas}`, true);
 }
-      // set token to header
-      xhr.setRequestHeader("Authorization", `Bearer ${token}`);
-      xhr.send();
 
 
+
+function FileActionHandler(data, SetFolderChain, curDirPath, SetCurDirPath, setFiles, bucket_name, selectedAtlas, token) {
+  // open a file picker UI
+  console.log('data', data)
+
+  if (data.id == "open_files" && data.payload.targetFile.isDir) {
+    var targetFile = data.payload.targetFile;
+    var targetFileChain = createFileChain(targetFile.id);
+
+    SetFolderChain(targetFileChain);
+
+    curDirPath = targetFile.id;
+    if (curDirPath == "/") {
+      curDirPath = "";
+    }
+    // if curDirPath does not end with "/", add it
+    if (!curDirPath.endsWith("/")) {
+      curDirPath = curDirPath + "/";
+    }
+    updateCurDirPath(curDirPath, SetCurDirPath);
+
+
+    console.log('listing bucket files', curDirPath)
+    ListBucketFiles(
+      setFiles,
+      bucket_name,
+      curDirPath,
+      token
+    );
+  }
+
+  if (data.id == "upload_files") {
+    // open
+
+    UploadFiles(curDirPath, setFiles, bucket_name, false, token);
+
+  }
+  if (data.id == "UploadFolder") {
+    // open
+    UploadFiles(curDirPath, setFiles, bucket_name, true, token);
+
+  }
+  // if (data.id == "create_folder") {
+  //   AddToItems(setItems, items, 'test_new', 'Processing') 
+  // }
+  if (data.id == "download_files") {
+    DownloadFiles(data, bucket_name, token);
+  }
+  if (data.id == "delete_files") {
+    DeleteFiles(data, curDirPath, setFiles, bucket_name, token);
+  }
+  if (data.id == "GenerateBrain") {
+    var selectedFiles = null;
+    // join all selected files with comma
+    for (var i = 0; i < data.state.selectedFilesForAction.length; i++) {
+      if (selectedFiles == null) {
+        selectedFiles = data.state.selectedFilesForAction[i].name;
+      } else {
+        selectedFiles =
+          selectedFiles + "," + data.state.selectedFilesForAction[i].name;
+      }
+    }
+    // set bucket name
+    // acquire brain ID from Input with ID brainID
+    var brainID = document.getElementById("brainIDInput").value;
+    // get the value of a select
+    console.log('brainID', brainID)
+    console.log('targetatlas', selectedAtlas)
+
+    // request to generate brain
+    var xhr = new XMLHttpRequest();
+    let redirect_uri = process.env.REACT_APP_URL;
+
+    console.log("CurDIRPath: " + curDirPath);
+    if (process.env.NODE_ENV === 'development') {
+      xhr.open("GET", `${redirect_uri}/tiffListToTarDZI?bucketname=${bucket_name}&filelist=${selectedFiles}&brainID=${brainID}&folderName=${curDirPath}&selectedAtlas=${selectedAtlas}`, true);
+    } else {
+      xhr.open("GET", `/tiffListToTarDZI?bucketname=${bucket_name}&filelist=${selectedFiles}&brainID=${brainID}&folderName=${curDirPath}&selectedAtlas=${selectedAtlas}`, true);
+    }
+    // set token to header
+    xhr.setRequestHeader("Authorization", `Bearer ${token}`);
+    xhr.send();
+
+
+  }
+}
+
+
+function MyChonkyTable(args) {
+  var folderChain = args.folderChain;
+  var SetFolderChain = args.SetFolderChain;
+  var curDirPath = args.curDirPath;
+  var SetCurDirPath = args.SetCurDirPath;
+  var files = args.files;
+  var setFiles = args.setFiles;
+  var token = args.token;
+  var bucket_name = args.currentBucket;
+  var selectedAtlas = args.selectedAtlas;
+  console.log('args', args)
+
+  function passToFileAction(SetFolderChain, curDirPath, SetCurDirPath, setFiles, bucket_name, selectedAtlas, token) {
+    return function (data) {
+      FileActionHandler(data, SetFolderChain, curDirPath, SetCurDirPath, setFiles, bucket_name, selectedAtlas, token);
     }
   }
- 
 
-  function MyChonkyTable(args) {
-    var folderChain = args.folderChain;
-    var SetFolderChain = args.SetFolderChain;
-    var curDirPath = args.curDirPath;
-    var SetCurDirPath = args.SetCurDirPath;
-    var files = args.files;
-    var setFiles = args.setFiles;
-    var token = args.token;
-    var bucket_name = args.currentBucket;
-    var selectedAtlas = args.selectedAtlas;
-    console.log('args', args)
-    
-    function passToFileAction (SetFolderChain, curDirPath, SetCurDirPath,setFiles,bucket_name, selectedAtlas,  token) {
-        return function (data) {
-            FileActionHandler(data, SetFolderChain, curDirPath, SetCurDirPath,setFiles,bucket_name,  selectedAtlas, token);
-        }
-    }
+  var button = document.querySelector('[title=" "]');
 
-    var button = document.querySelector('[title=" "]');
-    
-    // make button invisible
-    if (button != null) {
-        button.style.display = "none";
-    }
-    
-    
-  
+  // make button invisible
+  if (button != null) {
+    button.style.display = "none";
+  }
 
-    return (
-        <div id="uploadContainer">
-        <input
-        type="file"  
-        id="hiddenFileUploadButton"   
+
+
+
+  return (
+    <div id="uploadContainer">
+      <input
+        type="file"
+        id="hiddenFileUploadButton"
         // style={{ display: "none" }}
         multiple="true"
- 
+
 
       />
       {/* create directory upload button */}
@@ -253,11 +257,11 @@ function createFileChain(targetFilePath) {
         directory="true"
         multiple="true"
       />
- 
+
 
       <FileBrowser
         disableDefaultFileActions={true}
-        onFileAction={passToFileAction(SetFolderChain,curDirPath, SetCurDirPath,setFiles, bucket_name, selectedAtlas, token)}
+        onFileAction={passToFileAction(SetFolderChain, curDirPath, SetCurDirPath, setFiles, bucket_name, selectedAtlas, token)}
         files={files}
 
         defaultFileViewActionId={ChonkyActions.EnableListView.id}
@@ -269,13 +273,13 @@ function createFileChain(targetFilePath) {
         <FileNavbar />
 
         <FileToolbar />
-     
-  <FileList/>
+
+        <FileList />
         {/* <FileContextMenu /> */}
       </FileBrowser >
-        </div>
-    );
-    }
+    </div>
+  );
+}
 
 
 
